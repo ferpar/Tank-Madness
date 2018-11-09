07e9c2d4abe28a5901e3b9871063b5f861c5153c
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-09 01:33:03 +0100

    UI Improvements

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 window.onload = function () {
 
     document.querySelector('#start-button').onclick = function () {
+
+        if (document.querySelector('#names').checked){
+            document.querySelector('.Rover-label').innerHTML=prompt(`Player 1 Name`) + ": ";
+            document.querySelector('.Rover2-label').innerHTML=prompt(`Player 2 Name`) + ": ";
+        }
         game.init();
         document.querySelector('#start-button').setAttribute("style", "display:none");
     }
